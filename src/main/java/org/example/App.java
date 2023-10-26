c6cee79e5b91db74417369e0c8b07aba688935d3
aphelio22 <jrgaln.m@gmail.com>
2023-10-26 14:25:25 +0200

    Initial

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -3,7 +3,12 @@ package org.example;
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
+
+
+import java.util.ArrayList;
 
 /**
  * Hello world!
@@ -29,8 +34,16 @@ public class App
         try {
             //Operación de escritura (se hace igual con cualquier otra):
             Session session = sessionFactory.openSession();
+            //Necesario para hacer operaciones de escritura (junto a 'transaction.commit()'):
+            Transaction transaction = session.beginTransaction();
             //Es necesario mappear la info. de Usuario.
             session.persist(usuario);
+            transaction.commit();
+
+            Query query = session.createQuery("FROM Usuario");
+            ArrayList<Usuario> salida = (ArrayList<Usuario>) query.getResultList();
+
+            System.out.println(salida);
         } catch (HibernateException e) {
             throw new RuntimeException(e);
         }
